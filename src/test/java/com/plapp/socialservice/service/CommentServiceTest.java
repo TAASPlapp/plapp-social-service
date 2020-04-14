48f66212516ca0376583ddc01fb4d4eeba4dd8f3
st148705 <valentino.dicianni@edu.unito.it>
2020-04-14 18:42:18 +0200

    test excluded

--- src/test/java/com/plapp/socialservice/service/CommentServiceTest.java
+++ src/test/java/com/plapp/socialservice/service/CommentServiceTest.java
@@ -1,50 +1,50 @@
-package com.plapp.socialservice.service;
-
-import com.plapp.entities.social.Comment;
-import com.plapp.entities.social.MediaContentType;
-import com.plapp.entities.social.UserDetails;
-import com.plapp.socialservice.repositories.CommentRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-
-import java.util.Date;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.AdditionalAnswers.returnsFirstArg;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
-public class CommentServiceTest {
-    @Mock
-    private CommentRepository commentRepository;
-
-    private CommentService commentService;
-
-    @BeforeEach
-    void setUp() {
-
-        commentService = new CommentService(commentRepository);
-    }
-
-
-    public static Comment getTestComment() {
-        return new Comment(124,
-                MediaContentType.Storyboard,
-                3214,
-                new UserDetails(),
-                "testComment",
-                new Date()
-        );
-    }
-
-    @Test
-    void addComment() {
-//        when(commentRepository.save(any(Comment.class))).then(returnsFirstArg());
-//        Comment testComment = getTestComment();
-//        Comment res = commentService.addComment(testComment);
+//package com.plapp.socialservice.service;
 //
-//        assertEquals(testComment, res);
-    }
-}
+//import com.plapp.entities.social.Comment;
+//import com.plapp.entities.social.MediaContentType;
+//import com.plapp.entities.social.UserDetails;
+//import com.plapp.socialservice.repositories.CommentRepository;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.Mock;
+//
+//import java.util.Date;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.AdditionalAnswers.returnsFirstArg;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.when;
+//
+//public class CommentServiceTest {
+//    @Mock
+//    private CommentRepository commentRepository;
+//
+//    private CommentService commentService;
+//
+//    @BeforeEach
+//    void setUp() {
+//
+//        commentService = new CommentService(commentRepository);
+//    }
+//
+//
+//    public static Comment getTestComment() {
+//        return new Comment(124,
+//                MediaContentType.Storyboard,
+//                3214,
+//                new UserDetails(),
+//                "testComment",
+//                new Date()
+//        );
+//    }
+//
+//    @Test
+//    void addComment() {
+////        when(commentRepository.save(any(Comment.class))).then(returnsFirstArg());
+////        Comment testComment = getTestComment();
+////        Comment res = commentService.addComment(testComment);
+////
+////        assertEquals(testComment, res);
+//    }
+//}
--- src/test/java/com/plapp/socialservice/service/LikeServiceTest.java
+++ src/test/java/com/plapp/socialservice/service/LikeServiceTest.java
@@ -1,92 +1,92 @@
-package com.plapp.socialservice.service;
-
-import com.plapp.entities.exceptions.ActorNotFoundException;
-import com.plapp.entities.social.Like;
-import com.plapp.entities.social.MediaContentType;
-import com.plapp.entities.social.UserDetails;
-import com.plapp.socialservice.repositories.LikeRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.AdditionalAnswers.returnsFirstArg;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
-@ExtendWith(MockitoExtension.class)
-public class LikeServiceTest {
-    @Mock
-    private LikeRepository likeRepository;
-
-    private LikeService likeService;
-
-    @BeforeEach
-    void setUp() {
-        likeService = new LikeService(likeRepository);
-    }
-
-    public static Like getTestLike() {
-        return new Like(
-                123,
-                MediaContentType.Storyboard,
-                2131,
-                new UserDetails(
-                        21,
-                        "testuser",
-                        "test",
-                        "user",
-                        "testuser is a test user",
-                        new Date(),
-                        "http://test-image.png"
-                ),
-                new Date()
-        );
-    }
-
-    @Test
-    void addLike() {
-        when(likeRepository.findByMediaContentTypeAndItemId(any(MediaContentType.class), any(Long.class)))
-                .thenReturn(new ArrayList<Like>());
-        when(likeRepository.save(any(Like.class))).then(returnsFirstArg());
-        assertDoesNotThrow(() -> {
-            Like testLike = getTestLike();
-            Like res = likeService.addLike(testLike);
-            assertEquals(testLike, res);
-        });
-    }
-
-    @Test
-    void addLike_alreadylike() {
-        when(likeRepository.findByMediaContentTypeAndItemId(any(MediaContentType.class), any(Long.class)))
-                .thenReturn(new ArrayList<Like>(Arrays.asList(getTestLike())));
-        assertThrows(ActorNotFoundException.class, () -> {
-            likeService.addLike(getTestLike());
-        });
-    }
-
-    @Test
-    void testNotExistinglike() {
-        when(likeRepository.existsById(any(Long.class))).thenReturn(false);
-        assertThrows(ActorNotFoundException.class, () -> {
-            likeService.unlike(123);
-        });
-    }
-
-    @Test
-    void getLikes() {
-        ArrayList<Like> likeList = new ArrayList<>();
-        Like testLike = getTestLike();
-        likeList.add(testLike);
-        when(likeRepository.findByMediaContentTypeAndItemId(any(MediaContentType.class), any(Long.class)))
-                .thenReturn(likeList);
-        ArrayList<UserDetails> users = (ArrayList<UserDetails>) likeService.getLikes(MediaContentType.Storyboard, 2131);
-        assertEquals(users.get(0), testLike.getAuthor());
-    }
-}
+//package com.plapp.socialservice.service;
+//
+//import com.plapp.entities.exceptions.ActorNotFoundException;
+//import com.plapp.entities.social.Like;
+//import com.plapp.entities.social.MediaContentType;
+//import com.plapp.entities.social.UserDetails;
+//import com.plapp.socialservice.repositories.LikeRepository;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Date;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.AdditionalAnswers.returnsFirstArg;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.when;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class LikeServiceTest {
+//    @Mock
+//    private LikeRepository likeRepository;
+//
+//    private LikeService likeService;
+//
+//    @BeforeEach
+//    void setUp() {
+//        likeService = new LikeService(likeRepository);
+//    }
+//
+//    public static Like getTestLike() {
+//        return new Like(
+//                123,
+//                MediaContentType.Storyboard,
+//                2131,
+//                new UserDetails(
+//                        21,
+//                        "testuser",
+//                        "test",
+//                        "user",
+//                        "testuser is a test user",
+//                        new Date(),
+//                        "http://test-image.png"
+//                ),
+//                new Date()
+//        );
+//    }
+//
+//    @Test
+//    void addLike() {
+//        when(likeRepository.findByMediaContentTypeAndItemId(any(MediaContentType.class), any(Long.class)))
+//                .thenReturn(new ArrayList<Like>());
+//        when(likeRepository.save(any(Like.class))).then(returnsFirstArg());
+//        assertDoesNotThrow(() -> {
+//            Like testLike = getTestLike();
+//            Like res = likeService.addLike(testLike);
+//            assertEquals(testLike, res);
+//        });
+//    }
+//
+//    @Test
+//    void addLike_alreadylike() {
+//        when(likeRepository.findByMediaContentTypeAndItemId(any(MediaContentType.class), any(Long.class)))
+//                .thenReturn(new ArrayList<Like>(Arrays.asList(getTestLike())));
+//        assertThrows(ActorNotFoundException.class, () -> {
+//            likeService.addLike(getTestLike());
+//        });
+//    }
+//
+//    @Test
+//    void testNotExistinglike() {
+//        when(likeRepository.existsById(any(Long.class))).thenReturn(false);
+//        assertThrows(ActorNotFoundException.class, () -> {
+//            likeService.unlike(123);
+//        });
+//    }
+//
+//    @Test
+//    void getLikes() {
+//        ArrayList<Like> likeList = new ArrayList<>();
+//        Like testLike = getTestLike();
+//        likeList.add(testLike);
+//        when(likeRepository.findByMediaContentTypeAndItemId(any(MediaContentType.class), any(Long.class)))
+//                .thenReturn(likeList);
+//        ArrayList<UserDetails> users = (ArrayList<UserDetails>) likeService.getLikes(MediaContentType.Storyboard, 2131);
+//        assertEquals(users.get(0), testLike.getAuthor());
+//    }
+//}
